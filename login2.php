46b7948bbeac17e9ee3a0b001e300f1b7d1940fa
Keerdhana <111969941+Keerdhana@users.noreply.github.com>
2022-11-03 10:55:55 +0530

    Add files via upload

---
+++ login2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<div class="mt-5">
+		<title>WELCOME PAGE</title>
+	</head>
+	<body>
+      <div class="bg-dark">
+		<center>
+			<img src="guvilogo.png" style="width: 200px">
+		<div class="card mt-5" style="width: 50rem" align="center" >
+			<h1 class="center mt-3	">WELCOME to Guvi</h1>
+			<h2 class="center mt-3">Grab Your Vernacular Imprint</h2>
+			<h4 class="center mt-4">It Is An Online Platform To Learning The Programming Languages Like Python,Java,C,C++,Csharp,Boostrap,etc.....</h4>
+			<div class="center sm-3">
+			<button type="button" class="btn btn-primary ">START COURSE	
+			</button>
+			</div>
+
+			
+		</div>
+		</center>
+	</div>
+	</body>
+	</html>	
